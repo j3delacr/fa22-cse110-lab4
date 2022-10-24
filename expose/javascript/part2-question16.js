2cbc1382e1aab345897844e2b42d829537daeea9
javier <j3delacr@ucsd.edu>
2022-10-23 23:06:58 -0700

    updated question16.js

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,1 +1,10 @@
 
+for(var property in statistics) {
+    var first = property.substring(0, 1);
+    var num = statistics[property] % 2;
+
+    if((first == 'r') || (num == 1 )) {
+        console.log(statistics[property]);
+    }
+}
+
